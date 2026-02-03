f46d7aa44538aee1f4b3ba2b527b1ccff4d9c251
Lans12345 <olanalans12345@gmail.com>
2026-02-03 09:50:44 +0800

    fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f5d2364b620d02e462fc7aaabcc40b39
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "dd17449e3f1b479abdf7102b815a5e6a",
+"flutter_bootstrap.js": "c763f7b9e48e0b0b8186ccf435b23f7f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
